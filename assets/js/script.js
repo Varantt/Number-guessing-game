5a098c463aa17f4ee9a0891c68c3736270548b5b
Varantt <varantkalemkerian@gmail.com>
2023-03-19 23:39:28 +0200

    removed unnecessary log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 /**I want you to create a simple guess the number type game.
- It should choose a random number between 1 and 100, then challenge the player to guess the
-  number in 10 turns. After each turn, the player should be told if they are right or wrong,
-   and if they are wrong, whether the guess was too low or too high. It should also tell the
-    player what numbers they previously guessed. The game will end once the player guesses correctly, 
-    or once they run out of turns. When the game ends, the player should be given an option to start playing 
-    again.
-
+It should choose a random number between 1 and 100, then challenge the player to guess the
+number in 10 turns. After each turn, the player should be told if they are right or wrong,
+and if they are wrong, whether the guess was too low or too high. It should also tell the
+player what numbers they previously guessed. The game will end once the player guesses correctly, 
+or once they run out of turns. When the game ends, the player should be given an option to start playing 
+again.
     **/
+
 let resetBtn = document.querySelector(".reset-btn");
 let guessedNumber = Math.floor(Math.random() * 100) + 1;
 let previousGuesses = [];
@@ -36,7 +36,7 @@ const handleReset = () => {
   prompts.textContent = "";
   inputNumber.value = "";
   gameEnded = false;
-  resetBtn.classList.remove('d-block');
+  resetBtn.classList.remove("d-block");
 };
 resetBtn.addEventListener("click", handleReset);
 
@@ -46,16 +46,13 @@ const guessNumber = (e) => {
     return;
   }
 
-  console.log(inputNumber.value);
-  console.log(e);
   let input = parseInt(document.querySelector("input").value);
-  console.log(guessedNumber);
-  //   checking 10 turns
+
+  // checking 10 turns
   checkGameEnded(counter);
 
   counter++;
   previousGuesses.push(input);
-  console.log(previousGuesses);
 
   desc.classList.add("d-block");
   // If user guesses
